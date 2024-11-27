b8a8c4bf00b0b3970f7a694612996f344111bbfd
30000273 <30000273@sbstudents.org>
2024-11-27 11:35:39 -0500

    asdjke
    asd

--- Main.java
+++ Main.java
@@ -1,21 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-// i understand it now
-        System.out.println("hello sendy");
-
-        if (args.length != 2) {
-            System.out.println("please run with only two command line arguments; x y");
-            return;
-        }
-
-        MathOperation m = (int a, int b) -> a + b;
-        int x = m.calculate(Integer.valueOf(args[0]), Integer.valueOf(args[1]));
-        System.out.println("result: " + x);
-    }
-
-    @FunctionalInterface
-    interface MathOperation {
-        int calculate(int a, int b);
 		// Sendy y Faris
     }
 }
